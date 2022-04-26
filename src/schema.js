d4f10449e637fa35e9f1c0ed51266fe7531bfecf
Renan Sigolo <renan.sigolo@hellofresh.com.au>
2022-04-26 23:52:53 +1000

    feat: add woolworths market

---
+++ src/schema.js
@@ -0,0 +1,48 @@
+export class ProductColes {
+  constructor(userInput, productData) {
+    this.market = "Coles";
+    this.searchValue = userInput;
+    this.brand = productData.brand;
+    this.name = productData.name;
+    this.price = `${productData.priceDollar}${productData.priceCent}`;
+    this.quantity = productData.quantity
+      .toLowerCase()
+      .replace("approx.", "")
+      .trim();
+    this.packagePrice = productData.package
+      .split("per")[0]
+      .replace("$", "")
+      .trim();
+    this.packageUnit = productData.package.split("per")[1].trim();
+  }
+}
+
+export class ProductWoolworths {
+  constructor(userInput, productData) {
+    this.market = "Woolworths";
+    this.searchValue = userInput;
+    this.brand = productData.brand || "Not Available";
+    this.name = productData.name || "Not Available";
+    this.price =
+      productData.priceDollar && productData.priceCent
+        ? `${productData.priceDollar}.${productData.priceCent}`
+        : "Not Available";
+    this.quantity = "Not Available";
+    this.packagePrice = productData.package
+      ? productData.package.split("/")[0].replace("$", "").trim()
+      : "Not Available";
+    this.packageUnit = productData.package
+      ? productData.package.split("/")[1].trim()
+      : "Not Available";
+  }
+}
+
+export class ProductHarrisFarm {
+  constructor(userInput, productData) {
+    this.market = "Harris Farm";
+    this.searchValue = userInput;
+    this.brand = productData.brand;
+    this.name = productData.name;
+    this.price = `${productData.priceDollar}.${productData.priceCent}`;
+  }
+}
